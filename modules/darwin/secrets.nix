d6499fd7492933c4184020d66a858cf498fcc278
ClearAspect <roanmason@live.ca>
2025-02-17 21:54:45 -0500

    forced to update system.stateVersion to 5

--- modules/darwin/secrets.nix
+++ modules/darwin/secrets.nix
@@ -11,21 +11,21 @@ in {
     "/Users/${user}/.ssh/id_ed25519"
   ];
 
-  age.secrets."api-Claude" = {
-    symlink = true;
-    # path = "/run/agenix/api-Claude";
-    file = "${secrets}/api-Claude.age";
-    # mode = "600";
-    # owner = "${user}";
-    # group = "wheel";
-  };
+  # age.secrets."api-Claude" = {
+  #   symlink = true;
+  #   # path = "/run/agenix/api-Claude";
+  #   file = "${secrets}/api-Claude.age";
+  #   # mode = "600";
+  #   # owner = "${user}";
+  #   # group = "wheel";
+  # };
 
-  age.secrets."api-OpenAI" = {
-    symlink = true;
-    # path = "/run/agenix/api-OpenAI";
-    file = "${secrets}/api-OpenAI.age";
-    # mode = "600";
-    # owner = "${user}";
-    # group = "wheel";
-  };
+  # age.secrets."api-OpenAI" = {
+  #   symlink = true;
+  #   # path = "/run/agenix/api-OpenAI";
+  #   file = "${secrets}/api-OpenAI.age";
+  #   # mode = "600";
+  #   # owner = "${user}";
+  #   # group = "wheel";
+  # };
 }
